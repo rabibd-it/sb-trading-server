a82ca23ead098b098a40ea97eb898ee50a912974
Rabiul Islam <rabibd.it@gmail.com>
2021-11-12 21:01:38 +0600

    Check again

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://sbtrading:1aFqTb4nY5RD79Tt@cluster0.hikaa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hikaa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
